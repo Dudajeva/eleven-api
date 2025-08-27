97bcaf641750aedf700ba61f2bc5591b05ad2e3f
Dudajeva <1005793555@qq.com>
2025-08-27 16:07:48 +0800

    1

---
+++ eleven-api/src/test/java/org/adzc/elevenapi/ElevenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.adzc.elevenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
