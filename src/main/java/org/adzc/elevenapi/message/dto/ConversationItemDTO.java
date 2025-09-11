6fce43a1f4f2bf9936999981555f85a54c781ae4
Dudajeva <1005793555@qq.com>
2025-09-11 17:42:59 +0800

    Changes

--- src/main/java/org/adzc/elevenapi/message/dto/ConversationItemDTO.java
+++ src/main/java/org/adzc/elevenapi/message/dto/ConversationItemDTO.java
@@ -1,16 +1,19 @@
 package org.adzc.elevenapi.message.dto;
 
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
-public record ConversationItemDTO(
-        Long conversationId,
-        Long userId,
-        String nickname,
-        String avatarUrl,
-        String province,
-        String city,
-        String tier,
-        String lastMessage,
-        LocalDateTime lastTime,
-        Integer unread
-) {}
+@Data
+public class ConversationItemDTO {
+    private Long conversationId;
+    private Long userId;
+    private String nickname;
+    private String avatarUrl;
+    private String province;
+    private String city;
+    private String tier;
+    private String lastMessage;
+    private LocalDateTime lastTime;
+    private Integer unread;
+}
